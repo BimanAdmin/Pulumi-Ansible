feece7e2ae2788f17c8f6abc8c350d3dc967d4e8
bboro <bboro@unifocus.com>
2023-11-21 15:02:12 +0530

    new commit

--- winec2.ts
+++ winec2.ts
@@ -3,29 +3,44 @@ import * as pulumi from "@pulumi/pulumi";
 import * as aws from '@pulumi/aws';
 import { pubsub1id } from "./vpc";
 import { SecGrup } from "./securityGroup";
-import * as path from 'path';
 
-// Get the current directory
-const currentDir = __dirname;
+const region = "us-west-2";
 
-// // Local path to the script.ps1 file
-const localScriptPath = path.join(currentDir,'PULUMI-WINDOWS', 'script.ps1');
-
-// Script to create a folder and copy script.ps1 file
-const createFolderAndCopyScript = `
-$folderPath = "C:\\Ansible"
-if (-not (Test-Path $folderPath -PathType Container)) {
-    New-Item -Path $folderPath -ItemType Directory
+// Create a PowerShell script to be executed on the EC2 instance
+const folderScript = `
+Set-ExecutionPolicy -ExecutionPolicy Bypass -Force
+$folderPath = "C:\\PulumiFolder"
+if (!(Test-Path -Path $folderPath -PathType Container)) {
+    New-Item -ItemType Directory -Path $folderPath
 }
-
-$scriptPath = "${localScriptPath}"
-$destinationPath = Join-Path $folderPath "script.ps1"
-Copy-Item -Path $scriptPath -Destination $destinationPath -Force
 `;
 
-// Run the PowerShell script to create the folder and copy the script file
-const userData = pulumi.interpolate`<powershell>${createFolderAndCopyScript}</powershell>`;
+// Create an S3 bucket to store the PowerShell script
+const scriptBucket = new aws.s3.Bucket("script-bucket");
 
+// Upload the script.ps1 to the S3 bucket
+const scriptObject = new aws.s3.BucketObject("script.ps1", {
+    bucket: scriptBucket.bucket,
+    //acl: "public-read",
+    source: new pulumi.asset.FileAsset("script.ps1"),
+});
+
+// Download the script.ps1 file from S3 to the EC2 instance
+const downloadScript = new aws.s3.BucketObject("downloadScript.ps1", {
+    bucket: scriptBucket.bucket,
+    //acl: "private",
+    source: scriptObject,
+});
+
+// Set up user data to download and execute the script on the EC2 instance
+const userData = pulumi.interpolate`<powershell>
+${folderScript}
+# Download and execute the script.ps1
+Invoke-WebRequest -Uri ${downloadScript.bucket.apply(bucket => `https://${bucket}.s3.${region}.amazonaws.com/${downloadScript.key}`)} -OutFile C:\\PulumiFolder\\script.ps1
+# Grant read and execute permissions
+icacls C:\\PulumiFolder\\script.ps1 /grant:r "Users:(R,X)"
+C:\\PulumiFolder\\script.ps1
+</powershell>`;
 
 
 
@@ -38,7 +53,7 @@ const instance = new aws.ec2.Instance(varconfig.amzec2keyval.name, {
     associatePublicIpAddress: true,
     userData: userData,
     tags: {
-        Name: "WindowsInstanceWithScript",
+        Name: "Windows-server",
     },
 });
 
@@ -47,3 +62,4 @@ export const privateIp = instance.privateIp;
 export const publicHostName = instance.publicDns;
 
 export const instanceId = instance.id;
+export const scriptUrl = downloadScript.bucket.apply(bucket => `https://${bucket}.s3.${region}.amazonaws.com/${downloadScript.key}`);
