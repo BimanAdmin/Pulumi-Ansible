ea3c795c5ef40419858b65a7982ef7322fe9cc41
bboro <bboro@unifocus.com>
2023-11-21 12:57:53 +0530

    Latest commit

--- winec2.ts
+++ winec2.ts
@@ -3,49 +3,40 @@ import * as pulumi from "@pulumi/pulumi";
 import * as aws from '@pulumi/aws';
 import { pubsub1id } from "./vpc";
 import { SecGrup } from "./securityGroup";
+import * as path from 'path';
 
+// // Script to create a folder
+// const createFolderScript = `
+// New-Item -Path C:\\Ansible -ItemType Directory
+// `;
+// Get the current directory
+const currentDir = __dirname;
 
-// Script to create a folder
-const createFolderScript = `
-New-Item -Path C:\\Ansible -ItemType Directory
-`;
+// // Local path to the script.ps1 file
+const localScriptPath = path.join(currentDir,'PULUMI-WINDOWS', 'script.ps1');
 
-// Local path to the script.ps1 file
-const localScriptPath = "C:\\Users\\bboro\\Downloads\\script.ps1";
+// // Copy the script.ps1 file to the Windows EC2 instance
+// const copyFileScript = `
+// Copy-Item -Path ${localScriptPath} -Destination C:\\Ansible -Force
+// `;
 
-// Copy the script.ps1 file to the Windows EC2 instance
-const copyFileScript = `
-Copy-Item -Path ${localScriptPath} -Destination C:\\Ansible -Force
+// // Run the PowerShell script to create the folder and copy the script file
+// const userData = pulumi.interpolate`<powershell>${createFolderScript}\n${copyFileScript}</powershell>`;
+
+// Script to create a folder and copy script.ps1 file
+const createFolderAndCopyScript = `
+$folderPath = "C:\\Ansible"
+if (-not (Test-Path $folderPath -PathType Container)) {
+    New-Item -Path $folderPath -ItemType Directory
+}
+
+$scriptPath = "${localScriptPath}"
+$destinationPath = Join-Path $folderPath (Split-Path $scriptPath -Leaf)
+Copy-Item -Path $scriptPath -Destination $destinationPath -Force
 `;
 
 // Run the PowerShell script to create the folder and copy the script file
-const userData = pulumi.interpolate`<powershell>${createFolderScript}\n${copyFileScript}</powershell>`;
-
-
-// // Create an S3 bucket
-// const bucket = new aws.s3.Bucket("my-s3-bucket");
-
-// // Upload a local file to the S3 bucket
-// const object = new aws.s3.BucketObject("my-object", {
-//     bucket: bucket,
-//     //acl: "private", // Set the ACL as needed
-//     source: new pulumi.asset.FileAsset("C:\\Users\\bboro\\Downloads\\script.ps1"),
-// });
-
-
-
-//const userData1 = pulumi.interpolate`<powershell>${createFolderScript}\n${copyFileScript}</powershell>`;
-
-//const userData = pulumi.interpolate`<powershell>Start-Sleep -s 60; Invoke-WebRequest -Uri "${scriptObject.bucket.apply(bucket => `https://${bucket}.s3.amazonaws.com/script.ps1`)}" -OutFile C:\\Ansible\\script.ps1</powershell>`;
-
-// // Define the PowerShell script to download the file to the EC2 instance
-// const downloadFileScript = `
-// aws s3 cp s3://${bucket.bucket}/${object.key} C:\\Ansible\\
-// `;
-
-// Run a PowerShell script on the EC2 instance to download the file
-//const downloadFileUserData = pulumi.interpolate`<powershell>${downloadFileScript}</powershell>`;
-
+const userData = pulumi.interpolate`<powershell>${createFolderAndCopyScript}</powershell>`;
 
 
 const instance = new aws.ec2.Instance(varconfig.amzec2keyval.name, {
